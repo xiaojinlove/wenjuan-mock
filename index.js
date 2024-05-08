7e6591297d6e658f04a05aa84806ccc1b718eaf4
gaoyan <magaoyanxj@gmail.com>
2024-05-08 10:58:15 +0800

    使用nodejs启动服务，模拟get请求

---
+++ index.js
@@ -0,0 +1,26 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const mockList = require('./mock/index')
+
+const app = new Koa()
+const router = new Router()
+
+async function getRes(fn) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const res = fn()
+      resolve(res)
+    }, 1000);
+  })
+}
+//注册 mock 路由
+mockList.forEach(item => {
+  const { url, method, response } = item
+  router[method](url, async ctx => {
+    const res = await getRes(response) //模拟网络请求的加载状态，1s
+    ctx.body = res
+  })
+})
+
+app.use(router.routes())
+app.listen(3002)
