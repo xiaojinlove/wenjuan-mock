212b5083bf77e4a202fab70498805bd9cd06212d
gaoyan <magaoyanxj@gmail.com>
2024-05-09 20:20:11 +0800

    开发分页功能-定义URL参数page和pageSize

--- mock/question.js
+++ mock/question.js
@@ -39,13 +39,14 @@ module.exports = [
     url: '/api/question',
     method: 'get',
     response(ctx) {
-      const { url = '' } = ctx
+      const { url = '', query = {} } = ctx
       const isDeleted = url.indexOf('isDeleted=true') >= 0
       const isStar = url.indexOf('isStar=true') >= 0
+      const pageSize = parseInt(query.pageSize) || 10
       return {
           errno: 0,
           data: {
-            list: getQuestionList({ isDeleted, isStar}),
+            list: getQuestionList({ len: pageSize, isDeleted, isStar}),
             total: 100
           }
 
