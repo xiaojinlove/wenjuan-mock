1d2d7e6ea6423024441130a30f94e817bbf555d9
gaoyan <magaoyanxj@gmail.com>
2024-05-21 16:46:24 +0800

    答卷的数据结构设计-把结果写到Mock服务

--- mock/question.js
+++ mock/question.js
@@ -15,27 +15,95 @@ module.exports = [
             data: {
                 id: Random.id(),
                 title: Random.ctitle(),
+                desc: '问卷描述',
+                js: '',
+                css: '',
+                isPublished: true,
                 componentList: [
+                  {
+                    fe_id: Random.id(), 
+                    type: 'questionInfo', 
+                    title: '问卷信息',
+                    isHidden: false,
+                    isLocked: false,
+                    props: { title: '问卷标题', desc: '问卷描述' } 
+                  },
                   //Title
                   {
-                    id: Random.id(), 
+                    fe_id: Random.id(), 
                     type: 'questionTitle', 
                     title: '标题', 
+                    isHidden: false,
+                    isLocked: false,
                     props: { text: '个人信息调研', level: 1, isCenter: false } //组件类型不能重复，前后端统一好
                   },
                   //Input
                   {
-                    id: Random.id(), 
+                    fe_id: Random.id(), 
                     type: 'questionInput', 
                     title: '输入框1',
+                    isHidden: false,
+                    isLocked: false,
                     props: { title: '你的姓名', placeholder: '请输入姓名...' } 
                   },
+                  //Textarea
                   {
-                    id: Random.id(), 
+                    fe_id: Random.id(), 
+                    type: 'questionTextarea', 
+                    title: '多行输入',
+                    isHidden: false,
+                    isLocked: false,
+                    props: { title: '你的爱好', placeholder: '请输入...' } 
+                  },
+                  {
+                    fe_id: Random.id(), 
                     type: 'questionInput', 
                     title: '输入框2',
+                    isHidden: false,
+                    isLocked: false,
                     props: { title: '你的电话', placeholder: '请输入电话...' } 
-                  }
+                  },
+                  {
+                    fe_id: Random.id(), 
+                    type: 'questionParagraph', 
+                    title: '段落',
+                    isHidden: false,
+                    isLocked: false,
+                    props: { text: '一行段落', isCenter: false } 
+                  },
+                  {
+                    fe_id: Random.id(), 
+                    type: 'questionRadio', 
+                    title: '单选',
+                    isHidden: false,
+                    isLocked: false,
+                    props: { 
+                      title: '单选标题',
+                      isVertical: false,
+                      options: [
+                        { value: 'item1', text: '选项1' },
+                        { value: 'item2', text: '选项2' },
+                        { value: 'item3', text: '选项3' },
+                      ],
+                      value: '', 
+                    } 
+                  },
+                  {
+                    fe_id: Random.id(), 
+                    type: 'questionCheckbox', 
+                    title: '多选',
+                    isHidden: false,
+                    isLocked: false,
+                    props: { 
+                      title: '多选标题',
+                      isVertical: false,
+                      list: [
+                        { value: 'item1', text: '选项1', checked: false },
+                        { value: 'item2', text: '选项2', checked: false },
+                        { value: 'item3', text: '选项3', checked: false },
+                      ],
+                    } 
+                  },
                 ]
             }
 
